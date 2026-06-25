62c3ec4d81bc3d196f02ba1517b475cda66270f3
Manasa <jmanasareddy12@gmail.com>
2026-06-25 12:24:16 +0530

    Update DB connection to use env vars and SSL
    Refactor database connection to use environment variables and SSL settings.

--- models/db.py
+++ models/db.py
@@ -1,9 +1,14 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="de4dHuman",
-        database="spacemission"
+        host=os.environ["DB_HOST"],
+        port=int(os.environ.get("DB_PORT", 4000)),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        ssl_ca="isrgrootx1.pem",
+        ssl_verify_cert=True,
+        ssl_verify_identity=True
     )
